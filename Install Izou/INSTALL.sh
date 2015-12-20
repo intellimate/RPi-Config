8c0a00fb81e7e2336478c40330e22cccd8392eaf
Julian Brendl <julianbrendl@gmail.com>
2015-12-20 09:16:55 -0600

    added install izou files

---
+++ Install Izou/INSTALL.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "#!/bin/bash
+
+java -jar -DnoSecurity=true /izou/izou/izou.jar" >> izou-start.sh
+
+echo "#!/bin/bash
+pid=`ps aux | grep izou | awk '{print $2}'`
+kill -9 $pid" >> izou-stop.sh
+
+echo "#!/bin/bash
+# Izou
+#
+# description: Daemon for Izou
+
+case $1 in
+    start)
+        /bin/bash /usr/local/bin/izou-start.sh
+    ;;
+    stop)
+        /bin/bash /usr/local/bin/izou-stop.sh
+    ;;
+    restart)
+        /bin/bash /usr/local/bin/izou-stop.sh
+        /bin/bash /usr/local/bin/izou-start.sh
+    ;;
+esac
+exit 0" >> izou
+
+mv izou-start.sh /usr/local/bin/
+mv izou-stop.sh /usr/local/bin/
+mv izou /etc/init.d
+
+cd /etc/init.d
+chmod u+x izou
+update-rc.d izou defaults
+
+rmdir /home
---
+++ Install Izou/SETUP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is the setup file to setup the raspberry pi to run Izou. It creats a folder
+# in the root directory called izou and removes the home directory. This is
+# where izou will run from. 
+
+mkdir /izou
+mkdir /izou/izou
+mkdir /izou/config
+
+rmdir /home
