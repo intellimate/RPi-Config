d57425aef1e518db9c54b16e7268b7ffc8c2c8c7
Julian Brendl <julianbrendl@gmail.com>
2015-12-21 06:12:22 -0600

    fixed network monitor

--- Network Monitor/RUN.sh
+++ Network Monitor/RUN.sh
@@ -3,26 +3,20 @@
 # This script installs a service that periodically checks if there is internet,
 # and if there is not it reboots the network manager until internet is there again.
 
-echo "
+echo "#!/bin/bash
+
 #!/bin/bash
 
-# The IP for the server you wish to ping (8.8.8.8 is a public Google DNS server)
-SERVER=8.8.8.8
-
-# Only send two pings, sending output to /dev/null
-ping -c2 ${SERVER} > /dev/null
-
-# If the return code from ping ($?) is not 0 (meaning there was an error)
-if [ $? != 0 ]
-then
-    # Restart the wireless interface
-    ifdown --force wlan0
-    ifup wlan0
-fi
-" >> network-monitor.sh
+while true ; do
+   if ifconfig wlan0 | grep -q \"inet addr:\" ; then
+      sleep 60
+   else
+      echo \"Network connection down! Attempting reconnection.\"
+      ifup --force wlan0
+      sleep 10
+   fi
+done" >> network-monitor.sh
 
 mv network-monitor.sh /usr/local/bin
 
 chmod +x /usr/local/bin/network-monitor.sh
-
-echo "*/5 *   * * *   root    /usr/local/bin/network-monitor.sh" >> /etc/crontab
